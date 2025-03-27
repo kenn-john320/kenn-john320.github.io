fa66b2763e2370dac11e4f1c60e14bdfc773e093
Kennedy Ashley Johnson <kenn.john320@gmail.com>
2025-03-26 19:51:11 -0700

    Started project section and fixed slight scss issues

--- src/components/sections/ProjectSection/ProjectSection.tsx
+++ src/components/sections/ProjectSection/ProjectSection.tsx
@@ -3,11 +3,18 @@ import styles from './ProjectSection.module.scss'
 
 const ProjectSection: React.FC = () => {
     return (
-        <section>
-
-            <div className=''>
-                This is the Project Page
+        <section className={`${styles.sectionTitle}`}>
+            <div className={`${styles.container}`}>
+                <h3 className={`${styles.title}`}>
+                    This is the Project Page
+                </h3>
+            
+            <div className={`${styles.projectTitle}`}>
+                Featured Projects
+            </div>    
             </div>
+            
+
 
         </section>
 
